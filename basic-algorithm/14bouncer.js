d10c2b26fe8c7b445cc6f1f824e26e36d044079b
Alexander Shchegol <zelenzoom@gmail.com>
2017-06-21 15:56:54 +0300

    14 Bouncer

---
+++ basic-algorithm/14bouncer.js
@@ -0,0 +1,16 @@
+/**
+ * Created by admin on 21.06.2017.
+ *
+ * Falsy Bouncer
+ *
+ * Remove all falsy values from an array.
+ * Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+ */
+function bouncer(arr) {
+    return arr.filter(Boolean);
+}
+
+bouncer([7, "ate", "", false, 9]); // should return [7, "ate", 9].
+bouncer(["a", "b", "c"]); // should return ["a", "b", "c"].
+bouncer([false, null, 0, NaN, undefined, ""]); // should return [].
+bouncer([1, null, NaN, 2, undefined]); // should return [1, 2].
